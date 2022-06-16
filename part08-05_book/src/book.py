7197b9b8160824b3e031932aa8b137906cd5e08d
immortalRegis <69015233+immortalRegis@users.noreply.github.com>
2022-06-16 09:18:49 -0400

    Add files via upload
    second commit

---
+++ part08-05_book/src/book.py
@@ -0,0 +1,12 @@
+# Write your solution here:
+import string
+
+
+class Book:
+    def __init__(self, name: string, author: string, genre: string, year: int):
+        self.name = name
+        self.author = author
+        self.genre = genre
+        self.year = year
+
+
